a801e4abaa1e03d7d60168be78628c46d2d1eee8
Frederik Ellekilde <frederikellekilde@Frederiks-MacBook-Pro.local>
2017-09-19 19:03:16 +0200

    Oprettet user klasse

---
+++ src/Models/User.java
@@ -0,0 +1,37 @@
+package Models;
+
+import java.util.ArrayList;
+
+public class User {
+
+    String username;
+    String password;
+    ArrayList<String> skills;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword() {
+        this.password = password;
+    }
+
+    public ArrayList<String> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(ArrayList<String> skills) {
+        this.skills = skills;
+    }
+
+}
+
+
